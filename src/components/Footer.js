57ab07751686c25ba235a035bd4fbb09681bb7d3
Arghya Deep Pal <arghyadeep100@gmail.com>
2022-07-28 15:13:13 +0530

    Footer Updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,16 @@ import Button from 'react-bootstrap/Button';
 const Footer = () => {
   return (
     <Row style={{margin:"20px auto"}}>
-        <Col sm={6} style={{display:'flex', justifyContent: 'center'}}>
+        <Col sm={4} style={{display:'flex', justifyContent: 'center', marginTop:'10px'}}>
             <h5><i className="fas fa-copyright" />&nbsp;Made by ARGHYA DEEP PAL</h5>
         </Col>
-        <Col sm={6} style={{display:'flex', justifyContent: 'center'}}>
-          <Button variant="outline-danger" href="mailto:ronipal07@gmail.com?subject=Feedback for MOVIES APP" target="_blank">FEEDBACK</Button>
+        <Col sm={5} style={{display:'flex', justifyContent: 'center', marginTop:'10px'}}>
+          <a href="https://www.linkedin.com/in/arghya-deep-pal7/" style={{color:'blue', textDecoration: 'none'}} target="_blank"><i class="fab fa-linkedin fa-2x"></i>&nbsp;&nbsp;</a>
+          <a href="https://github.com/Arghyadeep7" style={{color:'black', textDecoration: 'none'}} target="_blank"><i class="fab fa-github fa-2x"></i>&nbsp;&nbsp;</a>
+          <a href="mailto:ronipal07@gmail.com" style={{color:'red', textDecoration: 'none'}} target="_blank"><i class="fas fa-envelope fa-2x"></i></a>
+        </Col>
+        <Col sm={3} style={{display:'flex', justifyContent: 'center', marginTop:'10px'}}>
+          <Button variant="outline-danger" href="mailto:ronipal07@gmail.com" target="_blank">CONTACT</Button>
         </Col>
     </Row>
   )
