c79292b2fe9d4f1bdf513a7421e82a4a34bdf095
Arghya Deep Pal <arghyadeep100@gmail.com>
2022-07-28 12:58:53 +0530

    Footer fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,18 @@
-import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 
 const Footer = () => {
   return (
-    <Container>
-        <div className="d-flex justify-content-between" style={{margin:"20px auto"}}>
+    <Row style={{margin:"20px auto"}}>
+        <Col sm={6} style={{display:'flex', justifyContent: 'center'}}>
             <h5><i className="fas fa-copyright" />&nbsp;Made by ARGHYA DEEP PAL</h5>
-            <Button variant="outline-info" href="mailto:ronipal07@gmail.com?subject=Feedback for MOVIES APP" target="_blank">FEEDBACK</Button>
-        </div>
-    </Container>
+        </Col>
+        <Col sm={6} style={{display:'flex', justifyContent: 'center'}}>
+          <Button variant="outline-danger" href="mailto:ronipal07@gmail.com?subject=Feedback for MOVIES APP" target="_blank">FEEDBACK</Button>
+        </Col>
+    </Row>
   )
 }
 
-export default Footer
+export default Footer;
