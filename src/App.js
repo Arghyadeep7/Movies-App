2eca619dd4bc7dd3ae9c30fdf0f465108e2f9ba1
Arghya Deep Pal <arghyadeep100@gmail.com>
2022-08-09 20:47:46 +0530

    Movies App Shifted to new URL

--- src/App.js
+++ src/App.js
@@ -1,32 +1,21 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import MovieComponent from "./components/MovieComponent";
-import MovieDetail from "./components/MovieDetail";
+import { Routes, Route } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 
-import SearchQuery from "./components/SearchQuery";
+
 
 function App() {
 
   return (
     <Container>
-      <Header/>
       <Routes>
-        <Route
-          path="/"
-          element={<Navigate replace to="/movies/now_playing" />}
-        />
-        <Route path="/movies/now_playing" element={<MovieComponent type="now_playing"/>} />
-        <Route path="/movies/upcoming" element={<MovieComponent type="upcoming"/>} />
-        <Route path="/movies/popular" element={<MovieComponent type="popular"/>} />
-        <Route path="/movies/top_rated" element={<MovieComponent type="top_rated"/>} />
-        <Route path="/movies/trending" element={<MovieComponent type="trending"/>} />
-        <Route path="/movie/:id" element={<MovieDetail />} />
-        <Route path="/search/:query" element={<SearchQuery />} />
-        <Route path="*" element={<h1>Error, this page doesnot exist !</h1>} />
+        <Route path="*" element={
+          <>
+            <br/>
+            <h1>The MOVIES App has been shifted&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://e-hub-arghyadeep7.vercel.app/">here</a></h1>
+            <h2><a href="https://e-hub-arghyadeep7.vercel.app/">https://e-hub-arghyadeep7.vercel.app/</a></h2>
+          </>
+        } />
       </Routes>
-      <Footer />
     </Container>
   );
 }
